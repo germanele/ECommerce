42335200264febb4c4fd8294b983f3973e72580b
germanele <71389563+germanele@users.noreply.github.com>
2021-08-22 18:20:36 -0300

    login redirect

--- js/login.js
+++ js/login.js
@@ -2,5 +2,9 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("btn_singin").addEventListener("click", function(){
+        
+        window.location.href = "home.html"
+    });
 
 });
